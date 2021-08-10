12b9d668032a81d6a627ec032da481fc926dd2d8
BENQ\Ponson.PC.Sun <ponson.pc.sun@benq.com>
2021-08-10 15:23:02 +0800

    1. Add column name ['id', 'campaign', 'model', 'time'] into DataFrame manually. 2. Sorting the results.

--- ExportParser.py
+++ ExportParser.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 
-model_assign_df = pd.read_csv(r"data\contacts.csv")
-model_count_df = model_assign_df.groupby(["campaign", "model"]).count()
-print(model_count_df)
-model_count_df.to_csv(r"output\model_assignments_count.csv")
+model_assign_df = pd.read_csv(r"data\contacts.csv", header=None, names=['id', 'campaign', 'model', 'time'])
+model_count_series = model_assign_df.groupby(["campaign", "model"])["id"].count()
+model_count_series = model_count_series.sort_values(ascending=False)
+print(model_count_series.head())
+model_count_series.to_csv(r"output\model_assignments_count.csv")
